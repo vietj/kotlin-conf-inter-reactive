f84eea5cd036757ebecada5b4e033b2658701b6f
Julien Viet <julien@julienviet.com>
2017-10-28 09:48:48 +0200

    Original app for movingrating demo

---
+++ src/main/kotlin/com/julienviet/movierating/Utils.kt
@@ -0,0 +1,21 @@
+package com.julienviet.movierating
+
+import io.vertx.ext.web.Route
+import io.vertx.ext.web.RoutingContext
+import io.vertx.kotlin.coroutines.dispatcher
+import kotlinx.coroutines.experimental.launch
+
+/**
+ * An extension method for simplifying coroutines usage with Vert.x Web routers
+ */
+fun Route.coroutineHandler(fn : suspend (RoutingContext) -> Unit) {
+  handler { ctx ->
+    launch(ctx.vertx().dispatcher()) {
+      try {
+        fn(ctx)
+      } catch(e: Exception) {
+        ctx.fail(e)
+      }
+    }
+  }
+}
