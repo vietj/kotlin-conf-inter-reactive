3c80d5a07f3839b6476278f3e548a89996137a9b
Julien Viet <julien@julienviet.com>
2017-11-01 06:33:15 -0700

    Improve toString on JsonEvent

--- src/main/kotlin/com/julienviet/interreactive/jsonparser/JsonEvent.kt
+++ src/main/kotlin/com/julienviet/interreactive/jsonparser/JsonEvent.kt
@@ -2,22 +2,26 @@ package com.julienviet.interreactive.jsonparser
 
 sealed class JsonEvent {
 
-  class StartObject : JsonEvent() {}
-  class EndObject : JsonEvent() {}
+  class StartObject : JsonEvent() {
+    override fun toString() = "{"
+  }
+  class EndObject : JsonEvent() {
+    override fun toString() = "}"
+  }
 
-  class StartArray : JsonEvent() {}
-  class EndArray : JsonEvent() {}
+  class StartArray : JsonEvent() {
+    override fun toString() = "["
+  }
+
+  class EndArray : JsonEvent() {
+    override fun toString() = "]"
+  }
 
   class Value<T>(val value: T?) : JsonEvent() {
-    override fun toString(): String {
-      return "$value"
-    }
+    override fun toString() = "$value"
   }
 
   class Member(val name: String) : JsonEvent() {
-    override fun toString(): String {
-      return "\"" + name + "\":"
-    }
+    override fun toString() = "\"" + name + "\":"
   }
-
 }
