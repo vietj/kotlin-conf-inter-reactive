c6ffd7c4842ee2f948ca35e7397672c71895aaa4
Julien Viet <julien@julienviet.com>
2017-11-02 14:27:24 -0700

    Rewrite tests to use coroutines instead of vertx-unit

--- src/test/kotlin/com/julienviet/movierating/MovieRatingTest.kt
+++ src/test/kotlin/com/julienviet/movierating/MovieRatingTest.kt
@@ -1,64 +1,72 @@
 package com.julienviet.movierating
 
 import io.vertx.core.Vertx
-import io.vertx.ext.unit.TestContext
-import io.vertx.ext.unit.junit.VertxUnitRunner
+import io.vertx.core.buffer.Buffer
+import io.vertx.ext.web.client.HttpResponse
 import io.vertx.ext.web.client.WebClient
 import io.vertx.kotlin.core.json.json
 import io.vertx.kotlin.core.json.obj
+import io.vertx.kotlin.coroutines.awaitResult
+import io.vertx.kotlin.coroutines.dispatcher
 import io.vertx.kotlin.ext.web.client.WebClientOptions
+import kotlinx.coroutines.experimental.runBlocking
 import org.junit.After
+import org.junit.Assert.assertEquals
 import org.junit.Before
 import org.junit.Test
-import org.junit.runner.RunWith
 
-@RunWith(VertxUnitRunner::class)
 class MovieRatingTest {
 
   lateinit var vertx: Vertx
   lateinit var client: WebClient
 
   @Before
-  fun setUp(ctx: TestContext) {
+  fun setUp() {
     vertx = Vertx.vertx()
-    vertx.deployVerticle(App(), ctx.asyncAssertSuccess())
     client = WebClient.create(vertx, WebClientOptions(defaultPort = 8080))
+    runBlocking(vertx.dispatcher()) {
+      awaitResult<String> { vertx.deployVerticle(App(), it) }
+    }
   }
 
   @After
-  fun tearDown(ctx: TestContext) {
-    vertx.close(ctx.asyncAssertSuccess())
+  fun tearDown() {
+    vertx.close()
   }
 
   @Test
-  fun testGetMovie(ctx: TestContext) {
-    client.get("/movie/starwars")
-      .send(ctx.asyncAssertSuccess {
-        ctx.assertEquals(200, it.statusCode())
-        ctx.assertEquals(json {
-          obj(
-            "id" to "starwars",
-            "title" to "Star Wars"
-          )
-        }, it.bodyAsJsonObject())
+  fun testGetMovie() {
+    runBlocking(vertx.dispatcher()) {
+      val result = awaitResult<HttpResponse<Buffer>> { client.get("/movie/starwars").send(it) }
+      assertEquals(200, result.statusCode())
+      assertEquals(result.bodyAsJsonObject(), json {
+        obj(
+          "id" to "starwars",
+          "title" to "Star Wars"
+        )
       })
+    }
   }
 
   @Test
-  fun testRateMovie(ctx: TestContext) {
-    client.post("/rate/starwars")
-      .setQueryParam("rating", "5")
-      .send(ctx.asyncAssertSuccess {
-        ctx.assertEquals(201, it.statusCode())
-      })
+  fun testRateMovie() {
+    runBlocking(vertx.dispatcher()) {
+      val result = awaitResult<HttpResponse<Buffer>> { client
+        .post("/rate/starwars")
+        .setQueryParam("rating", "5")
+        .send(it) }
+      assertEquals(201, result.statusCode())
+    }
   }
 
   @Test
-  fun testRateNotFoundMovie(ctx: TestContext) {
-    client.post("/rate/does-not-exists")
-      .setQueryParam("rating", "5")
-      .send(ctx.asyncAssertSuccess {
-        ctx.assertEquals(404, it.statusCode())
-      })
+  fun testRateNotFoundMovie() {
+    runBlocking(vertx.dispatcher()) {
+      val result = awaitResult<HttpResponse<Buffer>> { client
+        .post("/rate/does-not-exists")
+        .setQueryParam("rating", "5")
+        .send(it) }
+      assertEquals(404, result.statusCode())
+    }
   }
 }
